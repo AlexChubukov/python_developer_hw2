c1ffac733502c8369a1b38c0ce33f33bac1dd6a9
Alexey Chubukov <alexchubukov12345@gmail.com>
2020-04-26 23:44:54 +0300

    Ivan`s changes fixed

--- homework/My_logger.py
+++ homework/My_logger.py
@@ -18,37 +18,20 @@ handler_error = logging.FileHandler("error.log")
 handler_error.setFormatter(formatter)
 bad_log.addHandler(handler_error)
 
-
-
-
-def init_log(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        result = exception = None
-        try:
-            result = func(*args, **kwargs)
-            good_log.info(f"Поступил новый пациент: {args[0]}")
-            return result
-        except Exception as err:
-            bad_log.error(f"Ошибка записи пациента: {err}")
-            exception = err
-        if exception is not None:
-            raise exception
-    return wrapper
-
-
 def log(func):
     @wraps(func)
     def wrapper(*args, **kwargs):
-        result = exception = None
         try:
-            result = func(*args, **kwargs)
-            return result
+            func(*args, **kwargs)
+        except TypeError as err:
+            bad_log.error(f"Ошибка записи пациента: {err}")
+            raise err
+        except ValueError as err:
+            bad_log.error(f"Ошибка записи пациента: {err}")
+            raise err
         except Exception as err:
             bad_log.error(f"{err}")
-            exception = err
-        if exception is not None:
-            raise exception
+            raise err
     return wrapper
 
 
--- homework/patient.py
+++ homework/patient.py
@@ -1,33 +1,33 @@
-#from hw2.python_developer_hw2.homework import My_logger
 from homework import My_logger
+import logging
 import re
 
 class Patient:
+    """Класс содержит основные данные о пациенте"""
 
     first_name = NotImplemented
     last_name = NotImplemented
 
-
-    @My_logger.init_log
     def __init__(self, name, surname, birth_date, phone_number, doc_type, doc_number):
-        self.good_log = My_logger.logging.getLogger("logger_access")
-        self.bad_log = My_logger.logging.getLogger("logger_error")
-
-
-        self._first_name = name
-        self._last_name = surname
-        self.birth_date = birth_date
-        self.phone = phone_number
-        self.document_type = doc_type
-        self.document_id = doc_number
-
-    #def __del__(self):
-    #    self.handler_access.close()
-    #    self.handler_error.close()
-        #self.good_log.removeHandler(self.handler_access)
-        #self.bad_log.removeHandler(self.handler_error)
-        #del self.good_log
-        #del self.bad_log
+        self.good_log = logging.getLogger("logger_access")
+        self.bad_log = logging.getLogger("logger_error")
+        try:
+            self.first_name_ = name
+            self.last_name_ = surname
+            self.birth_date = birth_date
+            self.phone = phone_number
+            self.document_type = doc_type
+            self.document_id = doc_number
+            self.good_log.info(f"Поступил новый пациент: {self}")
+        except TypeError as err:
+            self.bad_log.error(f"Ошибка записи пациента: {err}")
+            raise err
+        except ValueError as err:
+            self.bad_log.error(f"Ошибка записи пациента: {err}")
+            raise err
+        except Exception as err:
+            self.bad_log.error(f"{err}")
+            raise err
 
     def __str__(self):
         out = f"{self._first_name} " \
@@ -42,7 +42,7 @@ class Patient:
 
     @property
     def first_name(self):
-        return self.__first_name
+        return self._first_name
 
     @first_name.setter
     @My_logger.log
@@ -52,7 +52,7 @@ class Patient:
 
     @property
     def last_name(self):
-        return self.__last_name
+        return self._last_name
 
     @last_name.setter
     @My_logger.log
@@ -61,143 +61,138 @@ class Patient:
 
 
     @property
-    def _first_name(self):
-        return self.__first_name
+    def first_name_(self):
+        return self._first_name
 
-    @_first_name.setter
-    def _first_name(self, name):
+    @first_name_.setter
+    def first_name_(self, name):
         if not isinstance(name, str):
-            raise TypeError("Неправильный тип данных поля first_name")
-        if name.isalpha() != True:
-            raise ValueError("Name must contain only characters")
-        self.__first_name = name
+            raise TypeError(f"Неправильный тип данных поля first_name {type(name)} is not str")
+        if not name.isalpha():
+            raise ValueError(f"Имя должно содержать только символы {name}")
+        self._first_name = name
 
     @property
-    def _last_name(self):
-        return self.__last_name
+    def last_name_(self):
+        return self._last_name
 
-    @_last_name.setter
-    def _last_name(self, surname):
+    @last_name_.setter
+    def last_name_(self, surname):
         if not isinstance(surname, str):
-            raise TypeError("Неправильный тип данных поля last_name")
-        if surname.isalpha() != True:
-            raise ValueError("Surname must contain only characters")
-        self.__last_name = surname
+            raise TypeError(f"Неправильный тип данных поля last_name {type(surname)} is not str")
+        if not surname.isalpha():
+            raise ValueError(f"Фамилия должна содержать только символы {surname}")
+        self._last_name = surname
 
     @property
     def birth_date(self):
-        return self.__birth_date
+        return self._birth_date
 
     @birth_date.setter
     @My_logger.log
     def birth_date(self, date):
         if not isinstance(date, str):
-            raise TypeError("Неправильный тип данных поля birth_date")
-        format1 = re.search("^(\d{2}[\-|\.| ]?){2}\d{4}$", date) is not None
-        format2 = re.search("^\d{4}[\-|\.| ]?\d{2}[\-|\.| ]\d{2}$", date) is not None
-        if not (format1 or format2):
-            raise ValueError("Неправильная дата рождения")
-        day, mounth, year = '', '', ''
-        if format1:
+            raise TypeError(f"Неправильный тип данных поля birth_date {type(date)} is not str")
+        format1 = re.search("^(\d{2}[\-|\.| ]?){2}\d{4}$", date) is None
+        format2 = re.search("^\d{4}[\-|\.| ]?\d{2}[\-|\.| ]\d{2}$", date) is None
+        if format1 and format2:
+            raise ValueError(f"Неправильная дата рождения {date}")
+        if not format1:
             day, mounth, year = date[0:2], date[3:5], date[6:]
             assert (2020 - int(year)) < 150, "Неправильная дата рождения"
         else:
             year, mounth, day = date[0:4], date[5:7], date[8:]
             assert (2020 - int(year)) < 150, "Неправильная дата рождения"
-        try:
-            self.birth_date
+        if hasattr(self, '_birth_date'):
             self.good_log.info(f"Изменение поля birth_date объекта {self}")
-            self.__birth_date = '-'.join([year, mounth, day])
-        except AttributeError:
-            self.__birth_date = '-'.join([year, mounth, day])
+        self._birth_date = '-'.join([year, mounth, day])
 
 
     @property
     def phone(self):
-        return self.__phone
+        return self._phone
 
     @phone.setter
     @My_logger.log
     def phone(self, phone):
         if not isinstance(phone, str):
-            raise TypeError("Неправильный тип данных поля phone")
+            raise TypeError(f"Неправильный тип данных поля phone {type(phone)} is not str")
         if re.search("[\+7|8]?([\(| |\-]?[\d]{3}[\)| |\-]?){2}([\-| ]?[\d]{2}[\-| ]?){2}", phone) is None:
-            raise ValueError("Неправильный номер телефона")
+            raise ValueError(f"Неправильный номер телефона {phone}")
         phone = ''.join(filter(str.isdigit, phone))
         if len(phone) < 11:
             phone = '7' + phone
         phone = f'+7-{phone[1:4]}-{phone[4:7]}-{phone[7:9]}-{phone[9:11]}'
-        try:
-            self.phone
+        if hasattr(self, '_phone'):
             self.good_log.info(f"Изменение поля phone объекта {self}")
-            self.__phone = phone
-        except AttributeError:
-            self.__phone = phone
+        self._phone = phone
 
     @property
     def document_type(self):
-        return self.__document_type
+        return self._document_type
 
     @document_type.setter
     @My_logger.log
     def document_type(self, document):
         if not isinstance(document, str):
-            raise TypeError("Неправильный тип данных поля document_type")
+            raise TypeError(f"Неправильный тип данных поля document_type {type(document)} is not str")
         document = document.lower()
         if document not in {"паспорт", "заграничный паспорт", "водительские права"}:
-            raise ValueError("Неправильный тип документов")
-        try:
-            self.document_type
+            raise ValueError(f"Неправильный тип документов {document}")
+        if hasattr(self, '_document_type'):
             self.good_log.info(f"Изменение поля document_type объекта {self}")
-            self.__document_type = document
-        except AttributeError:
-            self.__document_type = document
+        self._document_type = document
 
 
     @property
     def document_id(self):
-        return self.__document_id
+        return self._document_id
 
     @document_id.setter
     @My_logger.log
     def document_id(self, doc_number):
         if not isinstance(doc_number, str):
-            raise TypeError("Неправильный тип данных поля document_id")
+            raise TypeError(f"Неправильный тип данных поля document_id {type(doc_number)} is not str")
         if self.document_type == "паспорт":
             if re.search("^([\d][ |\-|\-|\/]?){10}$", doc_number) is None:
-                raise ValueError("Неправильный номер паспорта")
+                raise ValueError(f"Неправильный номер паспорта {doc_number}")
             doc_number = ''.join(filter(str.isdigit, doc_number))
             doc_number = doc_number[:4] + " " + doc_number[4:]
         if self.document_type == "заграничный паспорт":
             if re.search("^([\d][ |\-|\-|\/]?){9}$", doc_number) is None:
-                raise ValueError("Неправильный номер заграничного паспорта")
+                raise ValueError(f"Неправильный номер заграничного паспорта {doc_number}")
             doc_number = ''.join(filter(str.isdigit, doc_number))
             doc_number = doc_number[:2] + " " + doc_number[2:]
         if self.document_type == "водительские права":
             if re.search("^([\d][ |\-|\-|\/]?){10}$", doc_number) is None:
-                raise ValueError("Неправильный номер прав")
+                raise ValueError(f"Неправильный номер прав {doc_number}")
             doc_number = ''.join(filter(str.isdigit, doc_number))
             doc_number = doc_number[:2] + " " + doc_number[2:4] + " " + doc_number[4:]
-        try:
-            self.document_id
+        if hasattr(self, '_document_id'):
             self.good_log.info(f"Изменение поля document_id объекта {self}")
-            self.__document_id = doc_number
-        except AttributeError:
-            self.__document_id = doc_number
+        self._document_id = doc_number
 
 
-    @classmethod
-    def create(cls, name, surname, birth_date, phone_number, doc_type, doc_number):
-        return cls(name, surname, birth_date, phone_number, doc_type, doc_number)
+    @staticmethod
+    def create(*args):
+        return Patient(*args)
 
     def save(self, filename="Patients.csv"):
-        data = [self._first_name, self._last_name, self.birth_date, self.phone, self.document_type, self.document_id]
+        data = [self.first_name, self.last_name, self.birth_date, self.phone, self.document_type, self.document_id]
         data = ','.join(data)
         data += '\n'
         try:
-            with open(filename, 'a') as file:
+            with open(filename, 'a', encoding="utf-8") as file:
                 file.write(data)
             self.good_log.info(f"Пациент {self} записан в файл")
+        except FileExistsError as err:
+            self.bad_log.error(f'Файл {filename} уже существует. {err}')
+        except FileNotFoundError as err:
+            self.bad_log.error(f'Файла {filename} не существует. {err}')
+        except IsADirectoryError as err:
+            self.bad_log.error(f'{filename} это директория {err}')
+        except PermissionError as err:
+            self.bad_log.error(f'У вас недостаточно прав для операции записи. {err}')
         except Exception as err:
             self.bad_log.error(f"Ошибка записи в файл: {err}")
 
@@ -205,26 +200,23 @@ class Patient:
 class Iterator:
     """Итератор для прохождения по коллекции"""
 
-    def __init__(self, collection, filename, limit=None):
-        self.collection = collection
+    def __init__(self, filename, limit=None):
         self.limit = limit
-        self.file = open(filename, 'r')
+        self.file = open(filename, 'r',  encoding="utf-8")
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        try:
-            line = self.file.readline()
-            line = line.rstrip('\n')
-            assert line is not ""
-            if self.limit is not None:
-                assert self.limit != 0
-                self.limit -= 1
-            return Patient(*line.split(','))
-        except AssertionError:
+        line = self.file.readline()
+        line = line.rstrip('\n')
+        if line == "":
             raise StopIteration
-
+        if (self.limit is not None) and self.limit == 0:
+            raise StopIteration
+        if self.limit is not None:
+            self.limit -= 1
+        return Patient(*line.split(','))
 
     def __del__(self):
         self.file.close()
@@ -233,8 +225,8 @@ class Iterator:
 class PatientCollection:
 
     def __init__(self, filename='Patients.csv'):
-        self.good_log = My_logger.logging.getLogger("logger_access")
-        self.bad_log = My_logger.logging.getLogger("logger_error")
+        self.good_log = logging.getLogger("logger_access")
+        self.bad_log = logging.getLogger("logger_error")
         self.filename = filename
         self.data = self.read_patients_from_file(filename)
 
@@ -243,21 +235,19 @@ class PatientCollection:
 
 
     def __iter__(self):
-        return Iterator(self.data, self.filename)
+        return Iterator(self.filename)
 
     def limit(self, limit=None):
-        return Iterator(self.data,self.filename, limit)
+        return Iterator(self.filename, limit)
 
-
-    @staticmethod
-    def read_patients_from_file(filename):
+    def read_patients_from_file(self, filename):
         data = []
         try:
             with open(filename, 'r') as file:
                 for line in file:
-                    p = Patient.Patient(*line.split(','))
+                    p = Patient(*line.split(','))
                     data.append(p)
-        except Exception:
-            self.bad_log.error(f"Ошибка чтения пациентов из файла {filename}")
+        except Exception as err:
+            self.bad_log.error(f"Ошибка {err} чтения пациентов из файла {filename}")
         finally:
             return tuple(data)
